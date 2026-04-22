5c3aa3f4c566dc2d77381640f9a4b03c15321a9e
reftch <j.developer@hotmail.com>
2026-04-22 20:22:16 +0200

    Added cmake

---
+++ snippets/algorithm/fibonacci.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+unsigned long compute(unsigned long n) {
+    switch (n) {
+        case 0:
+        case 1:
+            return n;
+        default:
+            return compute(n - 1) + compute(n - 2);
+    }
+}
+
+int main() {
+    unsigned long n = 45;
+
+    std::cout << "Running fibonacci calculation..." << std::endl;
+    unsigned long result = compute(n);
+
+    std::cout << "Result fibonacci of " << n << " is " << result << std::endl;
+    return 0;
+}
