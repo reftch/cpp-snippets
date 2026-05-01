98d3202d26cc8803195d99e17ed799d5e7b635da
reftch <j.developer@hotmail.com>
2026-05-01 10:12:45 +0200

    Added async pools example

---
+++ snippets/parallel/async_pools.cpp
@@ -0,0 +1,32 @@
+#include <chrono>
+#include <future>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+int main() {
+    constexpr int pool_size = 10;
+    std::vector<std::future<int>> tasks;
+
+    // the lambda expression will be executed in a separate threads
+    auto lambda = [](int a) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+        return a * a;
+    };
+
+    // "Async pool" of std::async
+    std::cout << "Submitting async tasks...\n";
+    for (int i = 1; i <= pool_size; ++i) {
+        tasks.push_back(std::async(std::launch::async, lambda, i + 7));
+    }
+
+    // do something while async tasks run
+    std::cout << "Async tasks currently running; do other work..." << '\n';
+
+    // collect results from the pool
+    for (auto& f : tasks) {
+        std::cout << "result is " << f.get() << '\n';
+    }
+
+    return 0;
+}
